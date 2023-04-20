6512fa1ec6bbde953e84b6a54368d6093f1780a1
Crowdbotics <engineering@crowdbotics.com>
2023-04-20 16:32:36 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,23 @@
 import { StyleSheet } from "react-native"
+const duration = null
+const onDurationEnd = null
+const hide = () => SplashScreen.hide()
+const show = () => SplashScreen.show()
 
 export const globalOptions = {
   name: "test-appp-39533",
   url: "https://test-appp-39533.botics.co",
   api: "https://test-appp-39533.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
+  },
+  "@modules/splash": {
+    duration: duration,
+    onDurationEnd: onDurationEnd,
+    hide: hide,
+    show: show
   }
 }
